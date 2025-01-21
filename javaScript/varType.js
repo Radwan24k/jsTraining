70428e5f663f24881e16e355c380ba1cd37d40cd
Radwan24k <radwan.yusef.5s@gmail.com>
2025-01-21 12:43:40 +0200

    Add files via upload

---
+++ javaScript/varType.js
@@ -0,0 +1,11 @@
+var a = 10;
+var b = 20;
+var c = a * b;
+var d;
+var e = true;
+console.log("a is "+ typeof a);
+console.log("b is "+ typeof b);
+console.log("c = "+ c +" it is " + typeof c);
+console.log("d is "+ typeof d);
+console.log("e is "+ typeof e);
+
